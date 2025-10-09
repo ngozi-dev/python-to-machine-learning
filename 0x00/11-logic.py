39fe174f9103eda4c2b41d5356aec03994882fe1
ngozi-dev <franciscaezeaku@gmail.com>
2025-10-09 06:22:49 +0100

    python logic operator

---
+++ 0x00/11-logic.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+# a file that handles logic operations
+
+num = int(input("Enter any number of your choice"))
+print("logical AND: {}".format(num and 2))
+print("logical OR: {}".format(num or 2))
+print("logical NOT: {}".format(not num))
+
