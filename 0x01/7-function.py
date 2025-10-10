54765993e50225a4b089038b88dc6c6ab89c35c6
ngozi-dev <franciscaezeaku@gmail.com>
2025-10-10 11:14:10 +0100

    7-function.py

---
+++ 0x01/7-function.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# a module that declares and use functions
+
+def greet():
+    print("Hello, welcome to the world of python programming ")
+
+
+greet()
+
+def Sayhello(name):
+    print("Hello {}".format(name))
+
+Sayhello("John")
+Sayhello("Jane")
+Sayhello("Doe")
+
