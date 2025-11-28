aab38bc64310f5abf401f06fbc0f509ef87ce3c3
ngozi-dev <franciscaezeaku@gmail.com>
2025-11-28 20:57:26 +0100

    22-values.py

---
+++ 0x03/22-values.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# a module that displays values of the dictionary
+
+person = {
+    "name": "ngozi",
+    "age": 60,
+    "occupation": "civil servant",
+    "email": "franciscaezeaku@gmail.com"
+
+}
+
+print(person.values())
