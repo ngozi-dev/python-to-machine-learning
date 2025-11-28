e864ce1f08128dce44060eb2517f831ae47d923c
ngozi-dev <franciscaezeaku@gmail.com>
2025-11-28 20:56:22 +0100

    21-keys.py

---
+++ 0x03/21-keys.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# a module that displays a key in a dictionary
+
+car = {
+    "model": "2019",
+   "colour": "gold",
+    "brand": "toyota",
+    "milage": 7200,
+    "make": "camry"
+}
+
+print(car.keys())
