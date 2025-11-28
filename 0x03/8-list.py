c23c0f080cfb454a5b4d3aa6592e0a9e72c4bae7
ngozi-dev <franciscaezeaku@gmail.com>
2025-11-28 20:40:10 +0100

    8-list.py

---
+++ 0x03/8-list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# a module that performs a list operations
+
+fruits = ["Mango", "Orange", "Pawpaw", "Cucumba", "Cashew"]
+
+print(f"first item: {fruits[0]}")
+
+print(f"third item:{fruits[2]}")
+
+print(f"last item:{fruits[-1]}")
+
+num = [1, 2, 3, 4, 5]
+
+result = fruits + num
+print(result)
+
+
+
+
