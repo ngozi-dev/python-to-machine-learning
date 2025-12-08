7ed9076cdc0ac840f13ccf7e1b61539f148c7c30
ngozi-dev <franciscaezeaku@gmail.com>
2025-12-08 17:05:20 +0100

    1-write.py

---
+++ 0x04/1-write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# a module that write into a file
+
+with open ("example.txt", "w", encoding = "utf-8" ) as file:
+    file.write("python programming is a high level programming language.")
+
+
+       
