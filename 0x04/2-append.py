3de5f8cca94826e54d2dd32d6f935c5cf71b8485
ngozi-dev <franciscaezeaku@gmail.com>
2025-12-08 17:07:04 +0100

    2-append.py

---
+++ 0x04/2-append.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# a module that appends into a file
+
+with open("example.txt", "a", encoding="utf-8") as file:
+    file.write("\nIt is used for web development, data analysis, artificial intelligence, and more.")
+    
