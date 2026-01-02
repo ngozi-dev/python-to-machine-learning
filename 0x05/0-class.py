c545a13e11a0334ba5aeb593f24f35be9c730da1
ngozi-dev <franciscaezeaku@gmail.com>
2026-01-02 11:42:58 +0100

    0-class.py

---
+++ 0x05/0-class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# a module that creats a class
+
+class Square: # define a class with the keyword class in small letters
+    pass      # followed by a name that starts with a capital letter, 
+              # followed by colon
+if __name__ == "__main__": # to instantiate a class, you call the class
+    square = Square() # declare a variable square ie square in small letters to store the Square function
+    print(type(square)) # to display type of class wh is square
