5a956c305d6e1b4d9c382128a881d5c4b0e557b3
ngozi-dev <franciscaezeaku@gmail.com>
2026-01-10 07:56:17 +0100

    2-index.py

---
+++ 0x06/2-index.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# a module that displays the elements using index
+
+import numpy as np
+
+x = np.array([10, 20, 30, 40])
+print(x[0])
+print(x[1:3])
+print(x[-1])
+print(x[-2])
+
+
+
