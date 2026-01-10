dd2bd501a6f43f66c4eedb0e0a62f2940a19f3c6
ngozi-dev <franciscaezeaku@gmail.com>
2026-01-10 07:58:08 +0100

    4-operations.py

---
+++ 0x06/4-operations.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# a module that vectorise operations.
+
+import numpy as np
+
+x = np.array([1, 2, 3])
+y = np.array([10, 20, 30])
+
+
+print(x + y)
+print(x * y)
+print(x - y)
+print(x ** 2)
+print(np.sqrt(x))
