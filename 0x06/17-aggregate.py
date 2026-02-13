e9e170cf19dfdc6c22752cca607b9f65dbd0cddb
ngozi-dev <franciscaezeaku@gmail.com>
2026-02-13 10:12:58 +0100

    17-aggregate.py

--- 0x06/17-aggregate.py
+++ 0x06/17-aggregate.py
@@ -5,9 +5,9 @@ import pandas as pd
 
 df = pd.read_csv("Employee_Data.csv")
 df_grouped = df.groupby("Department")  # Group the DataFrame by the 'Department' column
-print(df_grouped["Department"].count())  # Count the number of records in each department
+#print(df_grouped["Department"].count())  # Count the number of records in each department
 print(df_grouped["Salary"].mean())  # Calculate the mean salary for each department
-print(df_grouped.agg({"Salary": ["min", "max", "mean"]}).sort_index())    # Aggregate min, max, and mean salary for each department
+#print(df_grouped.agg({"Salary": ["min", "max", "mean"]}).sort_index())    # Aggregate min, max, and mean salary for each department
 
 
 
