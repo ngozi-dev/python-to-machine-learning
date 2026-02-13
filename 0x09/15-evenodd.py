03ab95c557164f7a8a99513ac8437b8c09f6b59b
ngozi-dev <franciscaezeaku@gmail.com>
2026-02-13 11:01:25 +0100

    15-evenodd.py

---
+++ 0x09/15-evenodd.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# a module that shows whether a number is even or odd
+
+for i in range (1, 11):
+    if i % 2 == 0:
+        print(f"{i} is an even number. ")
+    else:
+        print(f"{i} is an odd number. ")
+        
