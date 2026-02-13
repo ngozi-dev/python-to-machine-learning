7d590e9b8571c8245137bf619744130f3d56180e
ngozi-dev <franciscaezeaku@gmail.com>
2026-02-13 10:40:17 +0100

    5-divide_numbers.py

--- 0x04/5-divide_numbers.py
+++ 0x04/5-divide_numbers.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 # a module that takes two numbers and returns their division
+def divide (num1, num2):
 
-try:
-    result = 7/0
+    try:
+        result = num1/num2
+        print(result)
+    except ZeroDivisionError:
+        print("Cannot divide by zero. Please provide a non-zero divisor. ")
+    except TypeError:
+        print("Invalid input. Please provide numbers for both numerator and divisor. ")
+
+if __name__ == "__main__":
+    result = divide(6,2)
     print(result)
-except ZeroDivisionError:
-    print("Cannot divide by zero. Please provide a non-zero divisor. ")
-except TypeError:
-    print("Invalid input. Please provide numbers for both numerator and divisor. ")
+
